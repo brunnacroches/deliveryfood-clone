f4b3a221d5286fb1f6106de89a8a8b406cec13ab
brunncroches <brunnacroches@gmail.com>
2023-05-29 11:43:10 -0300

    Starting Project

---
+++ screens/HomeScreen.js
@@ -0,0 +1,45 @@
+// HomeScreen.js
+import { useNavigation } from '@react-navigation/native';
+import React, { useLayoutEffect } from 'react';
+import { Image, SafeAreaView, Text, View } from 'react-native';
+import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className="bg-white">
+      <Text className="text-red-500">
+
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">
+                Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">Current Location
+                <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+
+          <UserIcon size={35} color="#00CCBB"/>
+        </View>
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
